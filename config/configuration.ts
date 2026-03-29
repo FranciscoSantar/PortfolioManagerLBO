6701f14ff702809caba82a558a10fc5cd4eea45b
Francisco Santarelli <franciscosantarelli@gmail.com>
2026-03-29 15:12:48 -0300

    feat: update configuration to include JWT and cache settings in .env and configuration files

--- config/configuration.ts
+++ config/configuration.ts
@@ -1,11 +1,17 @@
 export default (() => ({
   port: parseInt(process.env.PORT || '3000', 10),
-  is_prod: process.env.IS_PROD === 'true',
+  isProd: process.env.IS_PROD === 'true',
+  jwtSecret: process.env.JWT_SECRET,
+  jwtExpireTime: process.env.JWT_EXPIRES_IN || '1h',
   database: {
     host: process.env.DB_HOST || 'db',
     port: parseInt(process.env.DB_PORT || '5432', 10),
     name: process.env.DB_NAME || 'PortfolioManager',
     username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD
+  },
+  cache: {
+    host: process.env.CACHE_HOST || 'localhost',
+    port: process.env.CACHE_PORT || '6379'
   }
-}));
+}))
