c7136b25f9f788f4a224714458d4bfc87afefba3
Francisco Santarelli <franciscosantarelli@gmail.com>
2026-03-30 02:23:13 -0300

    feat: enhance YahooFinanceService with caching for price retrieval with TTL and batch processing for asset data. Created new methods for the retrival and caching update of a single price

--- src/yahoo-finance/yahoo-finance.service.ts
+++ src/yahoo-finance/yahoo-finance.service.ts
@@ -1,45 +1,134 @@
-import { Injectable } from '@nestjs/common';
 import YahooFinance from "yahoo-finance2";
+
+import { BadGatewayException, Inject, Injectable } from '@nestjs/common';
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+
 import { ShortAssetYahooFinance } from './interfaces/asset-yahoo-finance.interface';
-import { InsertAssetDto } from 'src/assets/dtos/insert-asset.dto';
+import { InsertAssetDto } from '../assets/dtos/insert-asset.dto';
+import { AssetPrice } from './interfaces/asset-price.interface';
 
 @Injectable()
 export class YahooFinanceService {
   private readonly yf = new YahooFinance();
   private readonly BATCH_SIZE = 20
+  private readonly PRICE_CACHE_TTL = 1000 * 60 * 5
 
+  constructor(
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
+
+  ) { }
   async getDataForSeeding(assetList: string[]) {
-    let yahooFinanceData: ShortAssetYahooFinance[] = []
+    let yahooFinanceShortData: ShortAssetYahooFinance[] = []
 
-    const assetsBatches = this.batchAssetList(assetList);
+    const data = await this.fetchYahooFinance(assetList)
+    const formattedData: ShortAssetYahooFinance[] = data.map((assetData) => ({
+      symbol: assetData.symbol,
+      longName: assetData.longName,
+      exchange: assetData.exchange,
+      currency: assetData.currency,
+      marketState: assetData.marketState
+    }))
+    yahooFinanceShortData.push(...formattedData)
 
-    for (const assetsBatch of assetsBatches) {
-      const data = await this.yf.quote(assetsBatch);
-      const formattedData: ShortAssetYahooFinance[] = data.map((assetData) => ({
-        symbol: assetData.symbol,
-        longName: assetData.longName,
-        exchange: assetData.exchange,
-        currency: assetData.currency,
-        marketState: assetData.marketState
-      }))
-      yahooFinanceData.push(...formattedData)
-    }
-    const insertAssetsDtos: InsertAssetDto[] = this.toInsertAssetDto(yahooFinanceData)
+    const insertAssetsDtos: InsertAssetDto[] = this.toInsertAssetDto(yahooFinanceShortData)
     return insertAssetsDtos;
   }
 
-  async getPriceByTicker() {
-    //TODO: Add caching
-    const ticker = 'USDT-USD';
-    const data = await this.yf.quote(ticker);
+  async getPriceByTicker(ticker: string) {
+    const assetPriceCacheKey = this.getPriceCachingKey(ticker)
+    const cachedPrice = await this.cacheManager.get<AssetPrice>(assetPriceCacheKey)
+    if (cachedPrice) {
+      return cachedPrice
+    }
+
+    const yahooFinanceData = await this.fetchYahooFinance([ticker])
+
+    if (!yahooFinanceData || !yahooFinanceData[0].regularMarketPrice) {
+      throw new BadGatewayException('Error fetching prices from the provider. Price is missing');
+    }
+
+    const yahooFinanceAssetPrice: AssetPrice = {
+      symbol: yahooFinanceData[0].symbol,
+      price: yahooFinanceData[0].regularMarketPrice,
+    }
+
+    await this.cacheManager.set(assetPriceCacheKey, yahooFinanceAssetPrice, this.PRICE_CACHE_TTL)
+    return yahooFinanceAssetPrice
+  }
+
+  async updatePriceByTicker(ticker: string) {
+    const assetPriceCacheKey = this.getPriceCachingKey(ticker)
+    await this.cacheManager.del(assetPriceCacheKey)
+
+    const yahooFinanceData = await this.fetchYahooFinance([ticker])
+    const yahooFinanceAssetPrice: AssetPrice = {
+      symbol: yahooFinanceData[0].symbol,
+      price: yahooFinanceData[0].regularMarketPrice,
+    }
+
+    await this.cacheManager.set(assetPriceCacheKey, yahooFinanceAssetPrice, this.PRICE_CACHE_TTL)
+
+    return true;
   }
 
   async getAllPrices(assetsList: string[]) {
-    //TODO: Add caching
-    const ticker = 'USDT-USD';
-    const data = await this.yf.quote(assetsList);
+    let assetsPrices: AssetPrice[] = []
+    let assetWithoutPrice: string[] = []
+
+    for (const asset of assetsList) {
+      const assetPriceCacheKey = this.getPriceCachingKey(asset)
+      const assetPrice = await this.cacheManager.get<AssetPrice>(assetPriceCacheKey)
+      if (!assetPrice) {
+        assetWithoutPrice.push(asset);
+        continue
+      }
+      assetsPrices.push(assetPrice)
+    }
+
+    if (assetWithoutPrice.length > 0) {
+      const yahooFinanceData = await this.fetchYahooFinance(assetWithoutPrice)
+
+      for (const yahooFinanceAsset of yahooFinanceData) {
+        if (!yahooFinanceAsset.regularMarketPrice) {
+          console.warn(`Error fetching prices from the provider. Price of ${yahooFinanceAsset.symbol} is missing`)
+          continue;
+        }
+        const yahooFinanceAssetPrice: AssetPrice = {
+          symbol: yahooFinanceAsset.symbol,
+          price: yahooFinanceAsset.regularMarketPrice,
+        }
+
+        assetsPrices.push(yahooFinanceAssetPrice)
+
+        const assetPriceCacheKey = this.getPriceCachingKey(yahooFinanceAssetPrice.symbol)
+        await this.cacheManager.set(assetPriceCacheKey, yahooFinanceAssetPrice, this.PRICE_CACHE_TTL)
+      }
+    }
+
+    return assetsPrices
   }
 
+  private async fetchYahooFinance(assetList: string[]) {
+    const yahooFinanceData: any[] = [];
+
+    try {
+      const assetsBatches = this.batchAssetList(assetList);
+
+      for (const assetsBatch of assetsBatches) {
+        const data = await this.yf.quote(assetsBatch);
+        yahooFinanceData.push(...data);
+      }
+    } catch {
+      throw new BadGatewayException('Error fetching prices from the provider');
+    }
+
+    if (yahooFinanceData.length === 0) {
+      throw new BadGatewayException('Error fetching prices from the provider');
+    }
+
+    return yahooFinanceData;
+  }
 
   private batchAssetList(assetList: string[]): string[][] {
     let batches: string[][] = []
@@ -63,5 +152,9 @@ export class YahooFinanceService {
     }))
 
     return insertAssetsDtos
+  }
+
+  getPriceCachingKey(ticker: string) {
+    return `${ticker}-price-key`
   }
 }
