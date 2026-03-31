6564f454c238b1628edcb9e0f27f5b5363974040
Francisco Santarelli <franciscosantarelli@gmail.com>
2026-03-31 17:54:13 -0300

    feat: add Swagger documentation for report generation endpoint in ReportsController

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get, Header, Param, ParseUUIDPipe, StreamableFile } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiUnauthorizedResponse } from '@nestjs/swagger';
+
 import { ReportsService } from './reports.service';
-import { User } from 'src/auth/decorators/user.decorator';
+import { User } from '../auth/decorators/user.decorator';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) { }
 
+  @ApiOperation({ summary: 'Generate an Excel report for a specific portfolio' })
+  @ApiOkResponse({ description: 'The Excel report has been successfully generated and is being downloaded.' })
+  @ApiBadRequestResponse({ description: 'Invalid portfolio ID' })
+  @ApiNotFoundResponse({ description: 'Portfolio not found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   @Get(':portfolioId')
   @Header('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
   @Header('Content-Disposition', 'attachment; filename="PortfolioReport.xlsx"')
