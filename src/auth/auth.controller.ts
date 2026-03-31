a99927652637c01610f0b5873ddf7f8cbdf59803
Francisco Santarelli <franciscosantarelli@gmail.com>
2026-03-31 16:28:51 -0300

    feat: add ApiOperation decorators for user registration and login endpoints to enhace documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiCreatedResponse, ApiOkResponse, ApiResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
@@ -12,6 +12,7 @@ import { LoginResponseDto, RegisterResponseDto } from './dto/response.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
+  @ApiOperation({ summary: 'Register a new user' })
   @ApiCreatedResponse({ description: 'User registered successfully', type: RegisterResponseDto })
   @ApiBadRequestResponse({ description: 'Invalid input data' })
   @Public()
@@ -20,6 +21,7 @@ export class AuthController {
     return this.authService.register(createUserDto);
   }
 
+  @ApiOperation({ summary: 'Login a user' })
   @ApiOkResponse({ description: 'User logged in successfully', type: LoginResponseDto })
   @ApiUnauthorizedResponse({ description: 'Invalid credentials' })
   @Public()
