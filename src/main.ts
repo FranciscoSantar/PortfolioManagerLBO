bd3731cca6ab6849371dbd02f867d2bb31368c79
Francisco Santarelli <franciscosantarelli@gmail.com>
2026-03-31 01:04:14 -0300

    refactor: simplify AppService by removing unused getHello method and update AppController to include health check endpoint. Configured a global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('/v1/api')
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
